a5a6b7338a708d7265bcd6b7702a7ba8a408a89d
Puri Calvo <puri_adrian@hotmail.com>
2024-10-28 19:43:51 +0100

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+
+
+export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState([ 'One Punch' ]);
+    
+    const onAddCategory = ( newCategory ) => {
+        
+        if ( categories.includes(newCategory) ) return;
+
+        setCategories( [ newCategory, ...categories ] );
+       
+    }
+
+  return (
+    <> 
+        {/* Titulo */}
+        <h1>GifExpertApp</h1>
+
+        {/* Input */}
+        <AddCategory 
+            // setCategories={ setCategories }
+            onNewCategory={ (value) => onAddCategory(value) }
+        />
+
+        {/* Listado de gif */}
+            { 
+                categories.map( (category) => ( 
+                        <GifGrid 
+                            key={ category } 
+                            category={ category }
+                        />
+                    )) 
+            }
+            {/* <li>ABC</li> */}
+       
+
+
+    </>
+  )
+}
